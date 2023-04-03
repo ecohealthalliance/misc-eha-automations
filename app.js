99aa668c01e4914e4db501a8b6dad27f1b34368e
espirado <andiespirado@gmail.com>
2023-04-03 14:08:11 +0300

    create webbhook on aws lambda

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const serverless = require("serverless-http");
+
+app.use(bodyParser.json());
+
+app.post("/webhook", async (req, res) => {
+  const event = req.body;
+  if (event.action === "created" && event.repository) {
+    try {
+      await triggerWorkflow(
+        event.repository.owner.login,
+        event.repository.name
+      );
+      res.sendStatus(200);
+    } catch (err) {
+      console.error(err);
+      res.sendStatus(500);
+    }
+  } else {
+    res.sendStatus(200);
+  }
+});
+
+async function triggerWorkflow(owner, repo) {
+  const token = process.env.GITHUB_TOKEN;
+  const workflowId = "add_all_team_to_new_repos.yml";
+
+  const url = `https://api.github.com/repos/${owner}/${repo}/actions/workflows/${workflowId}/dispatches`;
+
+  await axios.post(
+    url,
+    { ref: "main" },
+    {
+      headers: {
+        Authorization: `token ${token}`,
+        Accept: "application/vnd.github.v3+json",
+      },
+    }
+  );
+}
+
+module.exports.handler = serverless(app);
